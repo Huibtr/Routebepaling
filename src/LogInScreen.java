d2b1bcc721e89e4a003bd5d6ab793304162ce4f0
Nogikle <57618423+Nogikle@users.noreply.github.com>
2020-05-12 10:36:27 +0200

    loginlayout aangepast

--- src/LogInScreen.java
+++ src/LogInScreen.java
@@ -1,49 +1,85 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class  LogInScreen extends JFrame implements ActionListener {
+import javax.swing.*;
 
-    //Attributes
-    private JButton jbLogin;
-    private JPasswordField JPassword;
+/**
+ * This program demonstrates how to use JPanel in Swing.
+ * @author www.codejava.net
+ */
+public class LogInScreen extends JFrame implements ActionListener {
 
-    //Constructor
-    public LogInScreen(){
+    private JLabel labelUsername = new JLabel("Vul in gebruikersnaam: ");
+    private JLabel labelPassword = new JLabel("Vul in wachtwoord: ");
+    private JTextField textUsername = new JTextField(20);
+    private JPasswordField fieldPassword = new JPasswordField(20);
+    private JButton buttonLogin;
 
-        setTitle("Nerdy Gadgets | Log-in scherm");
-        setSize(500, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2,2));
+    public LogInScreen() {
+        super("Nerdy Gadgets");
 
-        JLabel lblPassword = new JLabel("Wachtwoord");
-        add(lblPassword);
+        // create a new panel with GridBagLayout manager
+        JPanel newPanel = new JPanel(new GridBagLayout());
 
-        JPassword = new JPasswordField();
-        add(JPassword);
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
 
-        jbLogin = new JButton("Log in");
-        jbLogin.addActionListener(this);
-        add(jbLogin);
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        newPanel.add(labelUsername, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(textUsername, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        newPanel.add(labelPassword, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(fieldPassword, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        buttonLogin = new JButton("Login");
+        buttonLogin.addActionListener(this);
+        newPanel.add(buttonLogin, constraints);
+
+        // set border for the panel
+        newPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Login scherm"));
+
+        // add the panel to this frame
+        add(newPanel);
+
+        pack();
+        setLocationRelativeTo(null);
 
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
         String wachtwoordBezorging = "Bezorging2020";
         String wachtwoordLogistiek = "Logistiek2020";
 
-        if(actionEvent.getSource() == jbLogin){
-            if(JPassword.getText().equals(wachtwoordBezorging)){
+        if(actionEvent.getSource() == buttonLogin){
+            if(fieldPassword.getText().equals(wachtwoordBezorging)){
                 RoutingScreen routingScreen= new RoutingScreen();
-            } else if (JPassword.getText().equals(wachtwoordLogistiek)){
-               DataScreen dataScreen = new DataScreen();
+                dispose();
+            } else if (fieldPassword.getText().equals(wachtwoordLogistiek)){
+                DataScreen dataScreen = new DataScreen();
+                dispose();
             } else {
                 JOptionPane.showMessageDialog(null, "Wachtwoord incorrect");
             }
         }
+
     }
 }
